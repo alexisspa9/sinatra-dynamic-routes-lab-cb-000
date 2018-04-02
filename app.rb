8ab46ec3a3d02c5510aa5c2152b1c151965133a0
Alexander Spanos <alexisspa9@gmail.com>
2018-04-02 08:50:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     "#{@name}"
   end
   get 'square/:number' do
-    @number = params[:number].to_i
+    (params[:number].to_i ** 2).to_s
 
   end
   get '/say/:number/:phrase' do
@@ -22,6 +22,20 @@ class App < Sinatra::Base
 "#{@words[0]} #{@words[1]} #{@words[2]} #{@words[3]} #{@words[4]} "
   end
   get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
 
+    answer = 'Unable to perform this operation'
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+    end
   end
 end
