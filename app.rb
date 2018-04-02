af84cd75007bb0658a1b6edfac31aa57707924cd
Alexander Spanos <alexisspa9@gmail.com>
2018-04-02 08:51:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   end
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @words = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]]
-"#{@words[0]} #{@words[1]} #{@words[2]} #{@words[3]} #{@words[4]} "
+"#{@words[0]} #{@words[1]} #{@words[2]} #{@words[3]} #{@words[4]}."
   end
   get '/:operation/:number1/:number2' do
     number1 = params[:number1].to_i
